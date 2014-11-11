acc6f744735fd53e141dc3930a7c1cfe07e08a19
MaximeHelaine <mhelainedu91@live.fr>
2014-11-11 21:50:30 +0100

    Création de la partie par un joueur "Ok"
    Me reste à faire en sorte que n'importe qui puisse voir les parties en
    cours et puisse en rejoindre une

---
+++ BrotherHood Rushers/Assets/Scripts/NetworkScripts/CreateServerScripts.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CreateServerScripts : MonoBehaviour {
+
+    public string _privateName = "";
+    public int _port = 21000;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void setPrivateName(InputField name)
+    {
+        _privateName = name.text;
+    }
+    public void setPort(InputField port)
+    {
+        _port = int.Parse(port.text);
+    }
+    public void StartServer()
+    {
+        Network.InitializeServer(32, _port, !Network.HavePublicAddress());
+        MasterServer.RegisterHost("BHR", _privateName, "Welcome to Brotherhood Runners");
+    }
+
+    public void OnServerInitialized()
+    {
+        Debug.Log("Server initialized");
+    }
+
+    public void OnMasterServerEvent(MasterServerEvent mse)
+    {
+	    if(mse == MasterServerEvent.RegistrationSucceeded)
+	    {
+            Debug.Log("Connection Succeful");
+	    }
+    }
+}
