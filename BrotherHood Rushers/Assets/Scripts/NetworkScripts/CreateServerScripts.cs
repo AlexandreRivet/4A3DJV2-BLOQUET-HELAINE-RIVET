4b821fe193b029ec5d692cd823c727d0b894ab03
MaximeHelaine <mhelainedu91@live.fr>
2014-11-15 23:26:31 +0100

    Ajout de sécurités et de commentaire dans le code des scripts de networking

--- BrotherHood Rushers/Assets/Scripts/NetworkScripts/CreateServerScripts.cs
+++ BrotherHood Rushers/Assets/Scripts/NetworkScripts/CreateServerScripts.cs
@@ -4,8 +4,12 @@ using System.Collections;
 
 public class CreateServerScripts : MonoBehaviour {
 
-    public string _privateName = "";
-    public int _port = 21000;
+    public int _maxConnection = 3;
+    
+    private string _privateName = " Game Name Empty";
+    private int _port = 21000;
+    
+
 	// Use this for initialization
 	void Start () {
 	
@@ -15,25 +19,48 @@ public class CreateServerScripts : MonoBehaviour {
 	void Update () {
 	
 	}
+
+    //Setter
+
     public void setPrivateName(InputField name)
     {
-        _privateName = name.text;
+        _privateName = name.text; //TODO: voir s'il est possible d'écraser une partie déjà existante, si oui mettre une sécurité
     }
     public void setPort(InputField port)
     {
-        _port = int.Parse(port.text);
+        if (int.TryParse(port.text, out _port))
+            _port = int.Parse(port.text); 
     }
+
+    //Getter
+
+    public string getPrivateName()
+    {
+        return _privateName;
+    }
+    public int setPort()
+    {
+        return _port;
+    }
+
+
+    //Others Functions
+
+    //Démarre le Serveur
     public void StartServer()
     {
+        //Network.InitializeSecurity(); //Permet de protéger son jeu des tricheurs :D
         Network.InitializeServer(32, _port, !Network.HavePublicAddress());
         MasterServer.RegisterHost("BHR", _privateName, "Welcome to Brotherhood Runners");
     }
 
+    //Fonction de debug appelée quand le serveur est initialisé
     public void OnServerInitialized()
     {
         Debug.Log("Server initialized");
     }
 
+    //Fonction de debug appelée quand le joueur qui a créé le serveur est connecté
     public void OnMasterServerEvent(MasterServerEvent mse)
     {
 	    if(mse == MasterServerEvent.RegistrationSucceeded)
@@ -41,8 +68,15 @@ public class CreateServerScripts : MonoBehaviour {
             Debug.Log("Connection Succeful");
 	    }
     }
+
+    //Fonction de debug appelée quand un nouveau joueur se connecte
     private void OnPlayerConnected(NetworkPlayer player)
     {
-        Debug.Log("Nex player");
+        Debug.Log("New player");
+        if (Network.connections.Length > _maxConnection)
+        {
+            Debug.Log("Trop de connection");
+            Network.CloseConnection(player, true);
+        }
     }
 }
