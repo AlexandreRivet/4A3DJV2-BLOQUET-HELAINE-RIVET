c0c92825082aad42024395266316d817268030b2
MaximeHelaine <mhelainedu91@live.fr>
2014-11-30 15:06:34 +0100

    Ajout de la possibilité de cliquer sur les éléments du décors pour, par la suite, intéragir ces derniers.
    Voir comment choisir la visibilité des actions possibles selon la classe
    ou le fait que l'objet avec lequel intéragir est caché.

---
+++ BrotherHood Rushers/Assets/Scripts/MenuScripts/OnClickObjectMenuScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnClickObjectMenuScript : MonoBehaviour {
+
+    public GameObject _objectPanelActive;
+    public GameObject _objectMenuActive;
+  
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseDown()
+    {
+        _objectPanelActive.SetActive(true);
+        _objectMenuActive.SetActive(true);
+    }
+}
