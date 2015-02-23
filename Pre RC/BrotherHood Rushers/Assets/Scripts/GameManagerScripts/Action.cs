1c99e4301da168c0a9e50c753a9329a10c67c2f7
MaximeHelaine <mhelainedu91@live.fr>
2015-02-23 21:13:09 +0100

    Ajout de la Pre RC pour voir l'avancement
    Pas encore totalement testé

---
+++ Pre RC/BrotherHood Rushers/Assets/Scripts/GameManagerScripts/Action.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+//Author: Bloquet Pierre
+//Date :28/11/2014
+//Last modification : 03/12/2014
+//Descrition:
+//Note:
+[System.Serializable]
+public class Action {
+	//Position de l'action
+    private int _idAction;
+    private int _idCharacter;
+    private string _typeAction;
+    private string _typeTarget;
+    private bool _moveBefore;
+    private float _secondToWait;
+    private float _xPositionTarget;
+    private ActionDatas _actionsData = null;
+	//Type de l'action effectuer
+    private int _actionState;
+
+	//initialize
+    public Action(int idCharacter, string typeAction, string typeTarget, bool moveBefore, float secondToWait, float xPositionTarget)
+    {
+        _idCharacter = idCharacter;
+        _typeAction = typeAction;
+        _typeTarget = typeTarget;
+        _moveBefore = moveBefore;
+        _secondToWait = secondToWait;
+        _xPositionTarget = xPositionTarget;
+        _actionsData = null;
+        _actionState = 0;
+	}
+  
+	//getter & setter
+    public int getIdAction()
+    {
+        return _idAction;
+    }
+    public int getIdCharacter()
+    {
+        return _idCharacter;
+    }
+    public string get_typeAction()
+    {
+        return _typeAction;
+    }
+    public string get_typeTarget()
+    {
+        return _typeTarget;
+    }
+    public bool get_moveBefore()
+    {
+        return _moveBefore;
+    }
+    public float get_secondToWait()
+    {
+        return _secondToWait;
+    }
+    public float get_xPositionTarget()
+    {
+        return _xPositionTarget;
+    }
+    public ActionDatas get_ActionsDatas()
+    {
+        return _actionsData;
+    }
+    public int get_actionState()
+    {
+        return _actionState;
+    }
+
+    public void set_IdAction(int value)
+    {
+        _idAction = value;
+    }
+    public void set_IdCharacter(int value)
+    {
+        _idCharacter = value;
+    }
+    public void set_typeAction(string value)
+    {
+        _typeAction = value;
+    }
+    public void set_typeTarget(string value)
+    {
+        _typeTarget = value;
+    }
+    public void set_moveBefore(bool value)
+    {
+        _moveBefore = value;
+    }
+    public void set_secondToWait(float value)
+    {
+        _secondToWait = value;
+    }
+    public void set_xPositionTarget(float value)
+    {
+        _xPositionTarget = value;
+    }
+    public void set_ActionsDatas(ActionDatas value)
+    {
+        _actionsData = value;
+    }
+    public void set_actionState(int state)
+    {
+        this._actionState = state;
+    }
+    
+}
